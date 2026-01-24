913054e72ccf69c83d7e832e8db20acd3ac693c5
Martin Kleine <24376955+datenzar@users.noreply.github.com>
2026-01-24 11:51:47 +0100

    chore: enforce 2-space indentation

--- src/plugin-config.ts
+++ src/plugin-config.ts
@@ -1,6 +1,7 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
-import { OhMyOpenCodeConfigSchema, type OhMyOpenCodeConfig } from "./config";
+
+import { type OhMyOpenCodeConfig, OhMyOpenCodeConfigSchema } from "./config";
 import { findProjectRoot } from "./hooks/rules-injector/finder";
 import {
 	addConfigLoadError,
